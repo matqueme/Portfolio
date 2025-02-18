6bfeb5526c6537cdd03fad83dda102520ac59a3d
Quentin <quentinsimler@gmail.com>
2025-02-19 00:57:35 +0100

    ⚰️

--- components/sections/OffersSections.vue
+++ components/sections/OffersSections.vue
@@ -135,68 +135,6 @@ onMounted(() => {
   };
 });
 
-// const animateTextShake = () => {
-//   console.log('shake');
-//   if (notreElement.value) {
-//     gsap.to(notreElement.value, {
-//       x: 10, // Déplace légèrement le texte sur l'axe X
-//       yoyo: true,
-//       repeat: 3, // Répète l'effet 3 fois
-//       duration: 0.1,
-//       ease: 'power1.out',
-//     });
-//   }
-// };
-
-// const resetTextShake = () => {
-//   if (notreElement.value) {
-//     gsap.to(notreElement.value, {
-//       x: 0, // Réinitialise la position
-//       duration: 0.3,
-//       ease: 'power1.out',
-//     });
-//   }
-// };
-
-// const animateTextGlitch = () => {
-//   if (notreElement.value) {
-//     gsap.to(notreElement.value, {
-//       x: () => gsap.utils.random(-5, 5), // Déplacement aléatoire sur l'axe X
-//       y: () => gsap.utils.random(-5, 5), // Déplacement aléatoire sur l'axe Y
-//       color: () => gsap.utils.random(['#ff0000', '#00ff00', '#0000ff']), // Changement de couleur aléatoire
-//       duration: 0.1,
-//       repeat: 10, // Répète l'animation 10 fois
-//       yoyo: true,
-//       ease: 'power1.inOut',
-//       onComplete: () => {
-//         gsap.to(notreElement.value, {
-//           x: 0,
-//           y: 0,
-//           color: 'var(--color-custom-red)', // Réinitialise la couleur
-//           duration: 0.2,
-//         });
-//       },
-//     });
-//   }
-// };
-// const animateTextSkew = () => {
-//   if (notreElement.value) {
-//     gsap.to(notreElement.value, {
-//       skewX: 20, // Inclinaison sur l'axe X
-//       skewY: 10, // Inclinaison sur l'axe Y
-//       duration: 0.5,
-//       ease: 'power2.out',
-//       onComplete: () => {
-//         gsap.to(notreElement.value, {
-//           skewX: 0,
-//           skewY: 0,
-//           duration: 0.5,
-//         });
-//       },
-//     });
-//   }
-// };
-
 const animateTextExplode = () => {
   if (notreElement.value) {
     const letters = notreElement.value?.textContent?.split('') || [];
