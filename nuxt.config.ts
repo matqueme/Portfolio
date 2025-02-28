c1ba77ae18b6bde5713cca11c164658e8bc0468f
Quentin <quentinsimler@gmail.com>
2025-02-28 14:52:22 +0100

    🔧 Update Google Analytics ID in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,7 +45,7 @@ export default defineNuxtConfig({
   },
   gtag: {
     enabled: process.env.NODE_ENV === 'production',
-    id: 'G-XXXXXXXXXX',
+    id: 'G-KND0BZG5MD',
   },
   schemaOrg: {},
 });
