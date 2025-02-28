45e3ccde14d16597ab799eab21b1d6445678a378
Quentin <quentinsimler@gmail.com>
2025-02-28 15:20:57 +0100

    🔧 Ajout variable d'environnement NUXT_PUBLIC_GTAG_ID pour la configuration de Google Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,7 +45,7 @@ export default defineNuxtConfig({
   },
   gtag: {
     enabled: process.env.NODE_ENV === 'production',
-    id: 'G-KND0BZG5MD',
+    id: process.env.NUXT_PUBLIC_GTAG_ID || '',
   },
   schemaOrg: {},
 });
